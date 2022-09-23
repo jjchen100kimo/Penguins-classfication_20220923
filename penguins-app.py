04f7e08b6c28e3c552adda84fdeaa4508a5b4c0b
jjchen100kimo <106857392+jjchen100kimo@users.noreply.github.com>
2022-09-23 20:36:26 +0800

    Update penguins-app.py
    from sklearn import datasets

--- penguins-app.py
+++ penguins-app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import seaborn as sb
+
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
